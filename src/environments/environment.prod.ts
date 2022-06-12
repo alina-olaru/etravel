19093065522238078d1a630eea0064ba6df1dda2
Alina Olaru <olarualina01@gmail.com>
2022-06-13 00:20:28 +0300

    Update prod endpoint

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://localhost:7025/api/',
+  apiUrl: 'https://etravelbackend.azurewebsites.net/api/',
   blockhainNetworkProvider: 'https://devnet-gateway.elrond.com',
   eTravelWallet: 'erd15y4qjtk7algu65zfp430eqdve9wk08uxxe5r97drsu5qna29vn0q89rsyg',
   tokenIdentifier: "ETVL-3cb6df"
